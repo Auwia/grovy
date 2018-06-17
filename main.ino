a4cf57a6cb59ada27003e108b498fdfc008984ec
Massimo Manganiello <massimo.manganiello@gmail.com>
2018-06-17 23:09:27 +0200

    Update main.ino
    ver 2.0
    new features / core

--- main.ino
+++ main.ino
@@ -1,32 +1,39 @@
-/* * Author: Massimo Manganiello * first development: 16/05/2018 * version: 1.0 */
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+/* * Author: Massimo Manganiello * */
+/* 16/05/2018: first development. * version: 1.0 */
+/* 17/06/2018: new core / feature. * version: 1.0 */
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
+#include <OneWire.h>
 #include <Adafruit_MCP3008.h>
 #include <Adafruit_MCP23017.h>
 #include <DallasTemperature.h>
-#include <Wire.h>
-#include <OneWire.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
 
 // PHASE : (GERMINATION, VEGETABLE_LOW, VEGETABLE_HIGH, FLOWERING_LOW, FLOWERING_HIGH)
 const char* PHASE = "GERMINATION";
 // LIGHT : (ON, OFF)
 const char* LIGHT = "ON";
-// WATER : (ON, OFF)
-const char* WATER = "OFF";
 
-// WIFI const
-char* ssid = "UPCA9E82C2";
-const char* wifi_password = "tp3Ya2mkhztk";
-WiFiClient client;
+// WIFI
+const char* ssid = "UPCA9E82C2";
+const char* password_wifi =  "tp3Ya2mkhztk";
+WiFiClient clientWIFI;
+
+// MQTT client
+const char* mqttServer = "192.168.0.178";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
 
 // MYSQL
 char* host = "e73173-mysql.services.easyname.eu";
 IPAddress server_addr(185, 51, 11, 3);
 char user[] = "u112031db1";
 char password[] = "4i340So";
-MySQL_Connection conn((Client *)&client);
+MySQL_Connection conn((Client *)&clientWIFI);
+MySQL_Cursor *cur_mem;
 
 // MCP3008
 Adafruit_MCP3008 adc;
@@ -46,9 +53,6 @@ int distance;
 #define ONE_WIRE_MAX_DEV 1 //The maximum number of devices 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-
-// TIME - 5 minutes timer
-int cont = 1;
 
 // MCP23017 PIN
 Adafruit_MCP23017 mcp0;
@@ -70,27 +74,47 @@ const int WATER_PUMP = 14;
 const int HEATER = 15;
 const int PELTIER = 16;
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-
-  // WIFI
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, wifi_password);
+String timestamp;
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() { 
+  Serial.begin(115200); 
+  
+  WiFi.begin(ssid, password_wifi); 
+  Serial.print("Connecting to WiFi.");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("ok.");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
 
-  // TEMPERATURE
+  Serial.print("Connecting to MQTT.");
+  while (!client.connected()) {
+    Serial.print(".");
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+      Serial.println("ok.");  
+    } else { 
+      Serial.println("failed with state: ");
+      Serial.print(client.state());
+      delay(2000); 
+    }
+  }
+ 
+  client.publish("esp/test", "Hello from ESP8266");
+  client.subscribe("water");
+  client.subscribe("light");
+  client.subscribe("phase");
+  client.subscribe("distance");
+  client.subscribe("water_level");
+  client.subscribe("light_spectrum");
+  client.subscribe("moisture");
+
+    // TEMPERATURE
   sensors.begin();
 
   // MCP23017
@@ -127,44 +151,89 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.println("OK");
-}
-void loop() {
-  // MYSQL
-  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
 
-  // TIME TIMESTAMP
-  String timestamp;
-  HTTPClient http;
-  http.begin("http://weinzuhause.altervista.org/ws/getDateTime.php");
-  int httpCode = http.GET();
-  if (httpCode > 0) {
-    timestamp = http.getString();
-    if (timestamp.substring(11, 12).toInt() >= 8 || timestamp.substring(11, 12).toInt() <= 19) {
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    message += (char)payload[i];
+  }
+  Serial.println("");
+  
+  if (String(topic).equals("water")) {
+    if(message.equals("1")) {
+      Serial.println("WATER->ON");
+      //mcp0.digitalWrite(WATER_PUMP, HIGH);
+    } else {
+      Serial.println("WATER->OFF");
+      //mcp0.digitalWrite(WATER_PUMP, LOW);      
+    }
+  }
+  
+  if (String(topic).equals("light")) {
+    if (message.equals("1")) {
+      Serial.println("LIGHT->ON");
       LIGHT = "ON";
     } else {
-      LIGHT = "OFF";
-    }
-    if ((timestamp.substring(11, 12).toInt() == 7 &&
-         timestamp.substring(14, 15).toInt() == 45) ||
-        (timestamp.substring(11, 12).toInt() == 19 && timestamp.substring(14, 15).toInt() == 45 ))
-    {
-      WATER = "ON";
-    } else {
-      WATER = "OFF";
-    }
-  } else {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      Serial.println("LIGHT->OFF");
+      LIGHT = "OFF";}
   }
-  http.end();
+    
+  if (String(topic).equals("phase")) {
+    switch ((char)payload[0]) {
+      // PHASE : (0: GERMINATION, 1: VEGETABLE_LOW, 2: VEGETABLE_HIGH, 3: FLOWERING_LOW, 4: FLOWERING_HIGH)
+      case '0':
+        Serial.println("PHASE=GERMINATION");
+        PHASE = "GERMINATION";
+        break;
+      case '1':
+        Serial.println("PHASE=VEGETABLE_LOW");
+        PHASE = "VEGETABLE_LOW";
+        break;
+      case '2':
+        Serial.println("PHASE=VEGETABLE_HIGH");
+        PHASE = "VEGETABLE_HIGH";
+        break;
+      case '3':
+        Serial.println("PHASE=FLOWERING_LOW");
+        PHASE = "FLOWERING_LOW";
+        break;
+      case '4':
+        Serial.println("PHASE=FLOWERING_HIGH");
+        PHASE = "FLOWERING_HIGH";
+        break;
+    }
+  }
+  if (String(topic).equals("distance")) {
+    Serial.print("distance: ");
+    float distance = getDistance();
+    Serial.println(distance);
+  }
+  if (String(topic).equals("water_level")) {
+    Serial.print("water_level: ");
+    float water_level = getWaterLevel();
+    Serial.println(water_level);
+  }
+  if (String(topic).equals("light_spectrum")) {
+    Serial.print("light_spectrum: ");
+    int light_spectrum = getLightSpectrum();
+    Serial.println(light_spectrum);
+  }
+  if (String(topic).equals("moisture")) {
+    Serial.print("moisture: ");
+    int moisture = getMoisture();
+    Serial.println(moisture);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+}
 
-  // TEMPERATURE
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  Serial.print("Temperature for the device 1 (index 0) is: ");
-  Serial.println(sensors.getTempCByIndex(0));
-
-  // RANGE SENSOR
+float getDistance() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -179,46 +248,66 @@ void loop() {
   // Prints the distance on the Serial Monitor
   Serial.print(timestamp + ": Distance: ");
   Serial.println(distance);
-
-  Serial.println((String)timestamp + ": MCP3008_ADC_0: " + adc.readADC(0)); // water level
-  Serial.println((String)timestamp + ": MCP3008_ADC_1: " + adc.readADC(1)); // moisture sensor
-  Serial.println((String)timestamp + ": MCP3008_ADC_2: " + adc.readADC(2)); // light spectrum
-  Serial.println((String)timestamp + ": MCP3008_ADC_3: " + adc.readADC(3));
-  Serial.println((String)timestamp + ": MCP3008_ADC_4: " + adc.readADC(4));
-  Serial.println((String)timestamp + ": MCP3008_ADC_5: " + adc.readADC(5));
-  Serial.println((String)timestamp + ": MCP3008_ADC_6: " + adc.readADC(6));
-  Serial.println((String)timestamp + ": MCP3008_ADC_7: " + adc.readADC(7));
-
-  if (cont == 5) {
-    Serial.println(timestamp + ": Recording data of water level.");
-    String UPDATE_SQL = (String)"update u112031db1.probe set status=" + adc.readADC(0) + " where name='water_level'";
-    cur_mem = new MySQL_Cursor(&conn);
-    cur_mem->execute(UPDATE_SQL.c_str());
-    delete cur_mem;
-  }
-
-  Serial.println(timestamp + ": Recording data of moisture.");
-  String INSERT_SQL = (String)"insert into u112031db1.mis_soil (timestamp, area, moisture) values (convert_tz(now(),@@session.time_zone,\'+02:00\'), \'Moisture\', " + adc.readADC(1) + ")";
+  Serial.print(timestamp + ": Recording distance...");
+  String INSERT_SQL = (String)"insert into u112031db1.mis_distance (timestamp, area, distance) values (convert_tz(now(),@@session.time_zone,\'+02:00\'), \'Distance\', " + distance + ")";
   cur_mem = new MySQL_Cursor(&conn);
   cur_mem->execute(INSERT_SQL.c_str());
   delete cur_mem;
+  Serial.println("done");
+  return distance;
+}
 
-  Serial.println(timestamp + ": Recording data of light spectrum.");
-  String UPDATE_SQL = (String)"update u112031db1.probe set status=" + adc.readADC(2) + " where name='light_spectrum'";
+float getWaterLevel() {
+  float water_level = adc.readADC(0);
+  Serial.println((String)timestamp + ": MCP3008_ADC_0: " + water_level); // water level
+  Serial.print(timestamp + ": Recording data of water level...");
+  String UPDATE_SQL = (String)"update u112031db1.probe set status=" + water_level + " where name='water_level'";
   cur_mem = new MySQL_Cursor(&conn);
   cur_mem->execute(UPDATE_SQL.c_str());
   delete cur_mem;
+  Serial.println("done");
+  return adc.readADC(0);
+  return water_level;
+}
 
-  if (cont == 5) {
-    Serial.println(timestamp + ": Recording distance.");
-    String INSERT_SQL = (String)"insert into u112031db1.mis_distance (timestamp, area, distance) values (convert_tz(now(),@@session.time_zone,\'+02:00\'), \'Distance\', " + distance + ")";
-    cur_mem = new MySQL_Cursor(&conn);
-    cur_mem->execute(INSERT_SQL.c_str());
-    delete cur_mem;
+float getLightSpectrum() {
+  float light_spectrum = adc.readADC(2);
+  Serial.println((String)timestamp + ": MCP3008_ADC_2: " + light_spectrum); // light spectrum
+  Serial.println(timestamp + ": Recording data of light spectrum.");
+  String UPDATE_SQL = (String)"update u112031db1.probe set status=" + light_spectrum + " where name='light_spectrum'";
+  cur_mem = new MySQL_Cursor(&conn);
+  cur_mem->execute(UPDATE_SQL.c_str());
+  delete cur_mem;
+  return light_spectrum;
+}
+
+int getMoisture() {
+  int moisture = adc.readADC(1);
+  Serial.println((String)timestamp + ": MCP3008_ADC_1: " + moisture); // moisture sensor
+  Serial.println(timestamp + ": Recording data of moisture.");
+  String INSERT_SQL = (String)"insert into u112031db1.mis_soil (timestamp, area, moisture) values (convert_tz(now(),@@session.time_zone,\'+02:00\'), \'Moisture\', " + moisture + ")";
+  cur_mem = new MySQL_Cursor(&conn);
+  cur_mem->execute(INSERT_SQL.c_str());
+  delete cur_mem;
+  return moisture;
+}
+
+void loop() {
+  client.loop();
+
+    // TIME TIMESTAMP
+  HTTPClient http;
+  http.begin("http://weinzuhause.altervista.org/ws/getDateTime.php");
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    timestamp = http.getString();
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
   }
+  http.end();
 
-  if (LIGHT == "ON") {
-    if (PHASE == "GERMINATION") {
+  if (LIGHT.equals("ON")) {
+    if (PHASE.equals("GERMINATION")) {
       mcp0.digitalWrite(LED_WARM_WHITE, HIGH);
       mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
       mcp0.digitalWrite(LED_ROYAL_BLUE, HIGH);
@@ -228,7 +317,7 @@ void loop() {
       mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
       mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
     }
-    if (PHASE == "VEGETABLE_LOW") {
+    if (PHASE.equals("VEGETABLE_LOW")) {
       mcp0.digitalWrite(LED_WARM_WHITE, LOW);
       mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
       mcp0.digitalWrite(LED_ROYAL_BLUE, HIGH);
@@ -238,7 +327,7 @@ void loop() {
       mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
       mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
     }
-    if (PHASE == "VEGETABLE_HIGH") {
+    if (PHASE.equals("VEGETABLE_HIGH")) {
       mcp0.digitalWrite(LED_WARM_WHITE, LOW);
       mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
       mcp0.digitalWrite(LED_ROYAL_BLUE, HIGH);
@@ -248,7 +337,7 @@ void loop() {
       mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
       mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
     }
-    if (PHASE == "FLOWERING_LOW") {
+    if (PHASE.equals("FLOWERING_LOW")) {
       mcp0.digitalWrite(LED_WARM_WHITE, LOW);
       mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
       mcp0.digitalWrite(LED_ROYAL_BLUE, LOW);
@@ -258,7 +347,7 @@ void loop() {
       mcp0.digitalWrite(LED_DEEP_RED_1, HIGH);
       mcp0.digitalWrite(LED_DEEP_RED_2, HIGH);
     }
-    if (PHASE == "FLOWERING_HIGH") {
+    if (PHASE.equals("FLOWERING_HIGH")) {
       mcp0.digitalWrite(LED_WARM_WHITE, LOW);
       mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
       mcp0.digitalWrite(LED_ROYAL_BLUE, LOW);
@@ -278,47 +367,4 @@ void loop() {
     mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
     mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
   }
-
-  if (WATER == "ON") {
-    mcp0.digitalWrite(WATER_PUMP, HIGH);
-  } else {
-    mcp0.digitalWrite(WATER_PUMP, LOW);
-  }
-
-  /*
-    mcp0.digitalWrite(0, HIGH);
-    delay(500);
-    mcp0.digitalWrite(0, LOW);
-    mcp0.digitalWrite(1, HIGH);
-    delay(500);
-    mcp0.digitalWrite(1, LOW);
-    mcp0.digitalWrite(2, HIGH);
-    delay(500);
-    mcp0.digitalWrite(2, LOW);
-    mcp0.digitalWrite(3, HIGH);
-    delay(500);
-    mcp0.digitalWrite(3, LOW);
-    mcp0.digitalWrite(4, HIGH);
-    delay(500);
-    mcp0.digitalWrite(4, LOW);
-    mcp0.digitalWrite(5, HIGH);
-    delay(500);
-    mcp0.digitalWrite(5, LOW);
-    mcp0.digitalWrite(6, HIGH);
-    delay(500);
-    mcp0.digitalWrite(6, LOW);
-
-    Serial.println((String)timestamp + ": MCP23017_PIN_8_READ: " + mcp0.digitalRead(8));
-    Serial.println((String)timestamp + ": MCP23017_PIN_9_READ: " + mcp0.digitalRead(9));
-  */
-
-  // 5 minutes
-  if (cont == 5) {
-    cont = 0;
-  }
-
-  delay(60000);
-
-  // 5 minutes
-  cont = cont + 1;
 }
