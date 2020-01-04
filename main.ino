93b4a3522ed589f382e56503e5d10446510427d8
Massimo Manganiello <massimo.manganiello@gmail.com>
2020-01-04 22:08:33 +0000

    3 digital meters + DS18B20 temperature sensors + fan out ON/OFF.
    04/01/2020: 3 digital meters + DS18B20 temperature sensors + fan out ON/OFF. 
    version: 1.2

--- main.ino
+++ main.ino
@@ -1,324 +1,224 @@
-/* * Author: Massimo Manganiello * first development: 16/05/2018 * version: 1.0 */
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <MySQL_Connection.h>
-#include <MySQL_Cursor.h>
-#include <Adafruit_MCP3008.h>
-#include <Adafruit_MCP23017.h>
-#include <DallasTemperature.h>
-#include <Wire.h>
-#include <OneWire.h>
+/* * Author: Massimo Manganiello * */
+/* 19/09/2019: New range sensore module, 3 digital meters. * version: 1.1 */
+/* 04/01/2020: 3 digital meters + DS18B20 temperature sensors + fan out ON/OFF. * version: 1.2 */
 
-// PHASE : (GERMINATION, VEGETABLE_LOW, VEGETABLE_HIGH, FLOWERING_LOW, FLOWERING_HIGH)
-const char* PHASE = "GERMINATION";
-// LIGHT : (ON, OFF)
-const char* LIGHT = "ON";
-// WATER : (ON, OFF)
-const char* WATER = "OFF";
+#include <ESP8266WiFi.h>              // WIFI
+#include <PubSubClient.h>             // MQTT
+#include <RemoteDebug.h>              // TELNET REMOTE DEBUG 
+#include <ESP8266mDNS.h>              // TELNET REMOTE DEBUG 
+#include <hcsr04.h>                   // RANGE SENSOR
+#include <ESP8266WebServer.h>         // REMOTE UPDATE OTA
+#include <ESP8266HTTPUpdateServer.h>  // REMOTE UPDATE OTA
+#include <OneWire.h>                  // TEMPERATURE SENSORS 1-WIRE DS18B20
+#include <DallasTemperature.h>        // TEMPERATURE SENSORS 1-WIRE DS18B20
 
-// WIFI const
-char* ssid = "UPCA9E82C2";
-const char* wifi_password = "tp3Ya2mkhztk";
-WiFiClient client;
+#define ECHO_PINa 16  // D0
+#define TRIG_PINa  5  // D1
+#define ECHO_PINb  4  // D2
+#define TRIG_PINb  0  // D3
+#define ECHO_PINc 12  // D6
+#define TRIG_PINc 13  // D7
 
-// MYSQL
-char* host = "e73173-mysql.services.easyname.eu";
-IPAddress server_addr(185, 51, 11, 3);
-char user[] = "u112031db1";
-char password[] = "4i340So";
-MySQL_Connection conn((Client *)&client);
 
-// MCP3008
-Adafruit_MCP3008 adc;
-#define CLOCK_PIN D5
-#define MISO_PIN D6
-#define MOSI_PIN D7
-#define CS_PIN D8
+// WIFI
+const char* ssid = "UPCA9E82C2";
+const char* password_wifi =  "tp3Ya2mkhztk";
+WiFiClient clientWIFI;
+
+// MQTT client
+const char* mqttServer = "192.168.0.159";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+PubSubClient clientMQTT(clientWIFI);
+
+// TELNET REMOTE DEBUG
+bool RemoteSerial = true; //true = Remote and local serial, false = local serial only
+RemoteDebug Debug;
+#define MAX_SRV_CLIENTS 1
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+const char* host = "esp8266-range_sensor";
 
 // RANGE SENSOR
-const int trigPin = 2; //D4
-const int echoPin = 0; //D3
-long duration;
-int distance;
+HCSR04 hcsr04a(TRIG_PINa, ECHO_PINa, 20, 4000);
+HCSR04 hcsr04b(TRIG_PINb, ECHO_PINb, 20, 4000);
+HCSR04 hcsr04c(TRIG_PINc, ECHO_PINc, 20, 4000);
 
-// TEMPERATURE
-#define ONE_WIRE_BUS 10 //Pin to which is attached a temperature sensor 
-#define ONE_WIRE_MAX_DEV 1 //The maximum number of devices 
+// REMOTE UPDATE OTA
+ESP8266WebServer httpServer(81);
+ESP8266HTTPUpdateServer httpUpdater;
+
+// TEMPERATURE SENSORS 1-WIRE DS18B20
+#define ONE_WIRE_BUS 14
 OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
-// TIME - 5 minutes timer
-int cont = 1;
-
-// MCP23017 PIN
-Adafruit_MCP23017 mcp0;
-const int FAN1IN = 0;
-const int FAN2IN = 1;
-const int FAN1OUT = 2;
-const int FAN2OUT = 3;
-const int FANLED = 4;
-const int CO2 = 5;
-const int LED_WARM_WHITE = 6;
-const int LED_FULL_SPECTRUM_UV = 7;
-const int LED_DEEP_RED_1 = 8;
-const int LED_DEEP_RED_2 = 9;
-const int LED_BRIGHT_RED = 10;
-const int LED_BRIGHT_BLUE_1 = 11;
-const int LED_BRIGHT_BLUE_2 = 12;
-const int LED_ROYAL_BLUE = 13;
-const int WATER_PUMP = 14;
-const int HEATER = 15;
-const int PELTIER = 16;
+DallasTemperature DS18B20(&oneWire);
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
 
   // WIFI
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, wifi_password);
+  WiFi.begin(ssid, password_wifi);
+  Serial.print("Connecting to WiFi.");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("ok.");
 
-  // TEMPERATURE
-  sensors.begin();
+  // MQTT client
+  clientMQTT.setServer(mqttServer, mqttPort);
+  clientMQTT.setCallback(callback);
 
-  // MCP23017
-  mcp0.begin(0);
-  mcp0.pinMode(0, OUTPUT);
-  mcp0.pinMode(1, OUTPUT);
-  mcp0.pinMode(2, OUTPUT);
-  mcp0.pinMode(3, OUTPUT);
-  mcp0.pinMode(4, OUTPUT);
-  mcp0.pinMode(5, OUTPUT);
-  mcp0.pinMode(6, OUTPUT);
-  mcp0.pinMode(7, OUTPUT);
-  mcp0.pinMode(8, OUTPUT);
-  mcp0.pinMode(9, OUTPUT);
-  mcp0.pinMode(10, OUTPUT);
-  mcp0.pinMode(11, OUTPUT);
-  mcp0.pinMode(12, OUTPUT);
-  mcp0.pinMode(13, OUTPUT);
-  mcp0.pinMode(14, OUTPUT);
-  mcp0.pinMode(15, OUTPUT);
+  // REMOTE UPDATE OTA
+  MDNS.begin(host);
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
+  MDNS.addService("http", "tcp", 81);
+  Serial.printf("HTTPUpdateServer ready! Open http://%d.local/update in your browser\n", host);
 
-  // MYSQL
-  if (conn.connect(server_addr, 3306, user, password)) {
-    delay(1000);
-  } else Serial.println("Connection failed.");
+  // TELNET REMOTE DEBUG
+  MDNS.addService("telnet", "tcp", 23);
+  Debug.begin(host);
+  Debug.setSerialEnabled(true);
+  Debug.setResetCmdEnabled(true); // Enable the reset command
+  rdebugIln("TELNET REMOTE DEBUG...ok!");
 
-  // MCP3008
-  Serial.print("MCP3008 init...");
-  adc.begin(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
-  Serial.println("OK");
+  // TEMPERATURE SENSORS 1-WIRE DS18B20
+  DS18B20.begin();
 
-  // RANGE SENSOR
-  Serial.print("HC-SR04 UltraSonic init...");
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.println("OK");
 }
+
 void loop() {
-  // MYSQL
-  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-
-  // TIME TIMESTAMP
-  String timestamp;
-  HTTPClient http;
-  http.begin("http://weinzuhause.altervista.org/ws/getDateTime.php");
-  int httpCode = http.GET();
-  if (httpCode > 0) {
-    timestamp = http.getString();
-    if (timestamp.substring(11, 12).toInt() >= 8 || timestamp.substring(11, 12).toInt() <= 19) {
-      LIGHT = "ON";
-    } else {
-      LIGHT = "OFF";
-    }
-    if ((timestamp.substring(11, 12).toInt() == 7 &&
-         timestamp.substring(14, 15).toInt() == 45) ||
-        (timestamp.substring(11, 12).toInt() == 19 && timestamp.substring(14, 15).toInt() == 45 ))
-    {
-      WATER = "ON";
-    } else {
-      WATER = "OFF";
-    }
-  } else {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  // MQTT client
+  if (!clientMQTT.connected()) {
+    rdebugDln("Reconnect to MQTT");
+    reconnect();
   }
-  http.end();
+  clientMQTT.loop();
 
-  // TEMPERATURE
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  Serial.print("Temperature for the device 1 (index 0) is: ");
-  Serial.println(sensors.getTempCByIndex(0));
+  // TELNET REMOTE DEBUG
+  if (RemoteSerial) Debug.handle();
 
-  // RANGE SENSOR
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print(timestamp + ": Distance: ");
+  // REMOTE UPDATE OTA
+  httpServer.handleClient();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!clientMQTT.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    rdebugI("Attempting MQTT connection...");
+    // Attempt to connect
+    if (clientMQTT.connect("ESP8266Client-range_sensor", mqttUser, mqttPassword)) {
+      Serial.println("connected");
+      rdebugI("connected");
+      clientMQTT.subscribe("distance_1");
+      clientMQTT.subscribe("distance_2");
+      clientMQTT.subscribe("distance_3");
+      clientMQTT.subscribe("distance_1_result");
+      clientMQTT.subscribe("distance_2_result");
+      clientMQTT.subscribe("distance_3_result");
+      clientMQTT.subscribe("temperature");        // TEMPERATURE SENSORS 1-WIRE DS18B20
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(clientMQTT.state());
+      Serial.println(" try again in 1 seconds");
+      rdebugE("failed, rc=");
+      rdebugE("" + clientMQTT.state());
+      rdebugEln(" try again in 1 seconds");
+      // Wait 5->1 seconds before retrying //BUG_001
+      // delay(5000);
+      delay(250);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  Serial.print("Message arrived in topic: ");
+  rdebugD("Message arrived in topic: ");
+  Serial.println(topic);
+  rdebugD("%s", topic);
+  Serial.print("Message:");
+  rdebugDln("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    // rdebugDln("%s", (char)payload[i]);
+    message += (char)payload[i];
+  }
+  Serial.println("");
+  delay(500);
+
+  if (String(topic).equals("distance_1")) {
+    getDistance1();
+  }
+  if (String(topic).equals("distance_2")) {
+    getDistance2();
+  }
+  if (String(topic).equals("distance_3")) {
+    getDistance3();
+  }
+  // TEMPERATURE SENSORS 1-WIRE DS18B20
+  if (String(topic).equals("temperature")) {
+    getTemperature();
+  }
+}
+
+void getDistance1() {
+  Serial.print("A: ");
+  rdebugD("A: ");
+  float distance = hcsr04a.distanceInMillimeters() / 10;
   Serial.println(distance);
+  rdebugDln("%f", distance);
+  char result[8];
+  char* message = dtostrf(distance, 6, 1, result);
+  int length = strlen(message);
+  boolean retained = true;
+  clientMQTT.publish("distance_1_result", (byte*)message, length, retained);
+}
+void getDistance2() {
+  Serial.print("B: ");
+  rdebugD("B: ");
+  float distance = hcsr04b.distanceInMillimeters() / 10;
+  Serial.println(distance);
+  rdebugDln("%f", distance);
+  char result[8];
+  char* message = dtostrf(distance, 6, 1, result);
+  int length = strlen(message);
+  boolean retained = true;
+  clientMQTT.publish("distance_2_result", (byte*)message, length, retained);
+}
+void getDistance3() {
+  Serial.print("C: ");
+  rdebugD("C: ");
+  float distance = hcsr04c.distanceInMillimeters() / 10;
+  Serial.println(distance);
+  rdebugDln("%f", distance);
+  char result[8];
+  char* message = dtostrf(distance, 6, 1, result);
+  int length = strlen(message);
+  boolean retained = true;
+  clientMQTT.publish("distance_3_result", (byte*)message, length, retained);
+}
+// TEMPERATURE SENSORS 1-WIRE DS18B20
+void getTemperature() {
+  int numberOfDevices = DS18B20.getDeviceCount();
+  rdebugIln("Number of device %d founds.", numberOfDevices);
+  DS18B20.requestTemperatures();
 
-  Serial.println((String)timestamp + ": MCP3008_ADC_0: " + adc.readADC(0)); // water level
-  Serial.println((String)timestamp + ": MCP3008_ADC_1: " + adc.readADC(1)); // moisture sensor
-  Serial.println((String)timestamp + ": MCP3008_ADC_2: " + adc.readADC(2)); // light spectrum
-  Serial.println((String)timestamp + ": MCP3008_ADC_3: " + adc.readADC(3));
-  Serial.println((String)timestamp + ": MCP3008_ADC_4: " + adc.readADC(4));
-  Serial.println((String)timestamp + ": MCP3008_ADC_5: " + adc.readADC(5));
-  Serial.println((String)timestamp + ": MCP3008_ADC_6: " + adc.readADC(6));
-  Serial.println((String)timestamp + ": MCP3008_ADC_7: " + adc.readADC(7));
-
-  if (cont == 5) {
-    Serial.println(timestamp + ": Recording data of water level.");
-    String UPDATE_SQL = (String)"update u112031db1.probe set status=" + adc.readADC(0) + " where name='water_level'";
-    cur_mem = new MySQL_Cursor(&conn);
-    cur_mem->execute(UPDATE_SQL.c_str());
-    delete cur_mem;
+  for (int i = 0; i < numberOfDevices; i++) {
+    rdebugD("Sensore ");
+    rdebugD("%d", i);
+    rdebugD(": ");
+    rdebugD("%f", DS18B20.getTempCByIndex(i));
+    rdebugD(" gradi C");
+    rdebugDln();
+    float temperature = DS18B20.getTempCByIndex(i);
+    String message = "Box_" + String(i) + "|" + String(temperature);
+    char charBuf[50];
+    int length = message.length();
+    message.toCharArray(charBuf, 50);
+    boolean retained = true;
+    clientMQTT.publish("temperature_result", (byte*)message.c_str(), length, retained);
   }
-
-  Serial.println(timestamp + ": Recording data of moisture.");
-  String INSERT_SQL = (String)"insert into u112031db1.mis_soil (timestamp, area, moisture) values (convert_tz(now(),@@session.time_zone,\'+02:00\'), \'Moisture\', " + adc.readADC(1) + ")";
-  cur_mem = new MySQL_Cursor(&conn);
-  cur_mem->execute(INSERT_SQL.c_str());
-  delete cur_mem;
-
-  Serial.println(timestamp + ": Recording data of light spectrum.");
-  String UPDATE_SQL = (String)"update u112031db1.probe set status=" + adc.readADC(2) + " where name='light_spectrum'";
-  cur_mem = new MySQL_Cursor(&conn);
-  cur_mem->execute(UPDATE_SQL.c_str());
-  delete cur_mem;
-
-  if (cont == 5) {
-    Serial.println(timestamp + ": Recording distance.");
-    String INSERT_SQL = (String)"insert into u112031db1.mis_distance (timestamp, area, distance) values (convert_tz(now(),@@session.time_zone,\'+02:00\'), \'Distance\', " + distance + ")";
-    cur_mem = new MySQL_Cursor(&conn);
-    cur_mem->execute(INSERT_SQL.c_str());
-    delete cur_mem;
-  }
-
-  if (LIGHT == "ON") {
-    if (PHASE == "GERMINATION") {
-      mcp0.digitalWrite(LED_WARM_WHITE, HIGH);
-      mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
-      mcp0.digitalWrite(LED_ROYAL_BLUE, HIGH);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_1, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_2, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_RED, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
-    }
-    if (PHASE == "VEGETABLE_LOW") {
-      mcp0.digitalWrite(LED_WARM_WHITE, LOW);
-      mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
-      mcp0.digitalWrite(LED_ROYAL_BLUE, HIGH);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_1, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_2, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_RED, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
-    }
-    if (PHASE == "VEGETABLE_HIGH") {
-      mcp0.digitalWrite(LED_WARM_WHITE, LOW);
-      mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
-      mcp0.digitalWrite(LED_ROYAL_BLUE, HIGH);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_1, HIGH);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_2, HIGH);
-      mcp0.digitalWrite(LED_BRIGHT_RED, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
-    }
-    if (PHASE == "FLOWERING_LOW") {
-      mcp0.digitalWrite(LED_WARM_WHITE, LOW);
-      mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
-      mcp0.digitalWrite(LED_ROYAL_BLUE, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_1, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_2, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_RED, HIGH);
-      mcp0.digitalWrite(LED_DEEP_RED_1, HIGH);
-      mcp0.digitalWrite(LED_DEEP_RED_2, HIGH);
-    }
-    if (PHASE == "FLOWERING_HIGH") {
-      mcp0.digitalWrite(LED_WARM_WHITE, LOW);
-      mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, HIGH);
-      mcp0.digitalWrite(LED_ROYAL_BLUE, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_1, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_BLUE_2, LOW);
-      mcp0.digitalWrite(LED_BRIGHT_RED, LOW);
-      mcp0.digitalWrite(LED_DEEP_RED_1, HIGH);
-      mcp0.digitalWrite(LED_DEEP_RED_2, HIGH);
-    }
-  } else {
-    mcp0.digitalWrite(LED_WARM_WHITE, LOW);
-    mcp0.digitalWrite(LED_FULL_SPECTRUM_UV, LOW);
-    mcp0.digitalWrite(LED_ROYAL_BLUE, LOW);
-    mcp0.digitalWrite(LED_BRIGHT_BLUE_1, LOW);
-    mcp0.digitalWrite(LED_BRIGHT_BLUE_2, LOW);
-    mcp0.digitalWrite(LED_BRIGHT_RED, LOW);
-    mcp0.digitalWrite(LED_DEEP_RED_1, LOW);
-    mcp0.digitalWrite(LED_DEEP_RED_2, LOW);
-  }
-
-  if (WATER == "ON") {
-    mcp0.digitalWrite(WATER_PUMP, HIGH);
-  } else {
-    mcp0.digitalWrite(WATER_PUMP, LOW);
-  }
-
-  /*
-    mcp0.digitalWrite(0, HIGH);
-    delay(500);
-    mcp0.digitalWrite(0, LOW);
-    mcp0.digitalWrite(1, HIGH);
-    delay(500);
-    mcp0.digitalWrite(1, LOW);
-    mcp0.digitalWrite(2, HIGH);
-    delay(500);
-    mcp0.digitalWrite(2, LOW);
-    mcp0.digitalWrite(3, HIGH);
-    delay(500);
-    mcp0.digitalWrite(3, LOW);
-    mcp0.digitalWrite(4, HIGH);
-    delay(500);
-    mcp0.digitalWrite(4, LOW);
-    mcp0.digitalWrite(5, HIGH);
-    delay(500);
-    mcp0.digitalWrite(5, LOW);
-    mcp0.digitalWrite(6, HIGH);
-    delay(500);
-    mcp0.digitalWrite(6, LOW);
-
-    Serial.println((String)timestamp + ": MCP23017_PIN_8_READ: " + mcp0.digitalRead(8));
-    Serial.println((String)timestamp + ": MCP23017_PIN_9_READ: " + mcp0.digitalRead(9));
-  */
-
-  // 5 minutes
-  if (cont == 5) {
-    cont = 0;
-  }
-
-  delay(60000);
-
-  // 5 minutes
-  cont = cont + 1;
 }
